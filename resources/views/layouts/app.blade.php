704a4dc7fa87889065fac0743d9cdf16bb82519b
sandraperezgu <sandra.perezgu@e-campus.uab.cat>
2018-12-29 16:31:53 +0100

    Tags sin insertar en BBBDD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,12 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
 <body>
     <div id="app">
@@ -78,5 +84,9 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
 </body>
 </html>
--- resources/views/new_post.blade.php
+++ resources/views/new_post.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tokenfield/0.12.0/css/bootstrap-tokenfield.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>-->
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -8,11 +15,11 @@
                 <div class="panel-heading">Create new Post</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('new_post') }}">
+                    <form class="form-horizontal" id="formCreatePost" method="POST" action="{{ route('new_post') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                            <label for="title" class="col-md-4 control-label">title</label>
+                            <label for="title" class="col-md-4 control-label">Title</label>
 
                             <div class="col-md-6">
                                 <textarea class="form-control" rows="2" name="title" id="title"></textarea>
@@ -25,7 +32,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-                            <label for="description" class="col-md-4 control-label">description</label>
+                            <label for="description" class="col-md-4 control-label">Description</label>
 
                             <div class="col-md-6">
                                 <textarea class="form-control" rows="5" name="description" id="description"></textarea>
@@ -37,8 +44,14 @@
                             </div>
                         </div>
                         <div class="form-group">
+                            <label class="col-md-4 control-label">Tags</label>
+                            <div class="col-md-6">
+                                <input type="text" name="tag" id="tag" class="form-control" />
+                            </div>
+                        </div>
+                        <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" id="submit" class="btn btn-primary">
                                     Create
                                 </button>
                             </div>
@@ -49,4 +62,59 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function(){
+
+        $('#tag').tokenfield({
+            autocomplete:{
+                source: ['PHP','Codeigniter','HTML','JQuery','Javascript','CSS','Laravel','CakePHP','Symfony','Yii 2','Phalcon','Zend','Slim','FuelPHP','PHPixie','Mysql'],
+                delay:100
+            },
+            showAutocompleteOnFocus: true
+        });
+
+        $('#formCreatePost').on('submit', function(event){
+            event.preventDefault();
+            if($.trim($('#title').val()).length == 0)
+            {
+                alert("Please enter Title");
+                return false;
+            }
+            else if($.trim($('#description').val()).length == 0)
+            {
+                alert("Please enter Tag(s)");
+                return false;
+            }
+            else
+            {
+                var form_data = $(this).serialize();
+                $('#submit').attr("disabled","disabled");
+                $.ajax({
+                    url:"insert.php",
+                    method:"POST",
+                    data:form_data,
+                    beforeSend:function(){
+                        $('#submit').val('Submitting...');
+                    },
+                    success:function(data){
+                        if(data != '')
+                        {
+                            $('#name').val('');
+                            $('#tag').tokenfield('setTokens',[]);
+                            $('#success_message').html(data);
+                            $('#submit').attr("disabled", false);
+                            $('#submit').val('Submit');
+                        }
+                    }
+                });
+                setInterval(function(){
+                    $('#success_message').html('');
+                }, 5000);
+            }
+        });
+
+    });
+</script>
+
 @endsection
